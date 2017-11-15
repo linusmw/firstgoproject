330cf302bdf59c89c278c74e6347152cdce9e6c0
Linus Woehrle <linusmwoehrle@gmail.com>
2017-11-15 20:12:37 +0100

    teilerberechnung

--- main.go
+++ main.go
@@ -2,31 +2,31 @@ package main
 
 import (
 	"fmt"
-	"bufio"
-	"os"
+	//"bufio"
+	//"os"
 )
+func main2() {
+	//reader := bufio.NewReader(os.Stdin)
+	//fmt.Print("Enter text: ")
+	//text, _ := reader.ReadString('\n')
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
 
-	fmt.Println("Papa ist der größte")
+
 
 	i := 0
 	for ; i < 1000; {
 		// furzkopf fred
 		//    for {
 		i = i + 1
-		if i >= 990 {
-			fmt.Println(i,text)
+		if i >= 1 {
+			fmt.Println(i,i%5)
 
 		}
 		//fmt.Println(i, "10§ zu bezahlen:   du duumme nuss")
 	}
-	fmt.Println("wir sind die stärksten")
-	fmt.Println("Papa ist der größte")
-	fmt.Println("Papa ist der größte")
-	fmt.Println("Papa ist der größte")
+
+	//fmt.Println("wir sind die stärksten")
+	//fmt.Println("Papa ist der größte")
+	//fmt.Println("Papa ist der größte")
+	//fmt.Println("Papa ist der größte")
 }
